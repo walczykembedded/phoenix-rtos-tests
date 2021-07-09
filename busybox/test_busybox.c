0c498aa89e5caf11ed609d62b0e9e0e480b86cee
damian.loewnau <damian.loewnau@gerrit.phoesys.com>
2021-07-09 13:13:36 +0200

    busybox: add a check if the test suite is in the bin directory
    JIRA: RTOS-59

--- busybox/test_busybox.c
+++ busybox/test_busybox.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-// #include <time.h>
+#include <dirent.h>
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
 	int ret;
+	DIR* dir = opendir("/bin/testsuite");
+
+	if (dir) {
+		closedir(dir);
+	} else if (ENOENT == errno) {
+		fprintf(stderr, "There is no busybox test suite to run, build project with \"long_test\"\n");
+		return 1;
+	} else {
+		fprintf(stderr, "There is problem with opening existing /bin/testsuite directory\n");
+		return 1;
+	}
 
 	if ((ret = vfork()) < 0) {
 		fprintf(stderr, "vfork function failed \n");
@@ -16,17 +28,17 @@ int main(int argc, char **argv)
 		fprintf(stderr, "exec function failed \n");
 		return 1;
 	}
-	printf("Posixsrv runned in background\n");
+	printf("Posixsrv ran in background\n");
+
+	/* without this delay sometimes posixsrv is not yet running before the next function call */
 	usleep(500000);
 
 	if ((ret = system("cd /bin/testsuite/ && export PATH=/bin:/sbin:/usr/bin:/usr/sbin && ./runtest")) < 0) {
 		fprintf(stderr, "system function failed \n");
 		return 1;
 	}
-	// system("echo -ne \"qwe\nasd\nzxc\n\" > input");
-	// system("echo -ne \"asd\" | eval \"diff -u - input\"");
 
-	printf("\nruntest done\n\n");
+	printf("\nRuntest done\n\n");
 
 	return 0;
 }
